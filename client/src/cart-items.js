c802783da93c86b920660b93ede2e4793e535ea4
Luroz <luisrpirela@gmail.com>
2021-03-15 13:47:08 -0400

    cart item more

--- client/src/cart-items.js
+++ client/src/cart-items.js
@@ -1,35 +1,53 @@
 export default [
   {
     id: 1,
-    title: "Papa",
+    title: "Potato",
     price: 50,
     img:
       "http://www.gstatic.com/hostedimg/a0646086d261aecb_landing",
     amount: 1,
-    
+    stock: 10,
+    type: 'vegetal'
   },
   {
     id: 2,
-    title: "Lechuga ",
+    title: "Letuce ",
     price: 50,
     img:
       "http://www.gstatic.com/hostedimg/9512445ef2453d03_landing",
-    amount: 1
+    amount: 1,
+    stock: 10,
+    type: 'vegetal'
   },
   {
     id: 3,
-    title: "Cebolla",
+    title: "Onion",
     price: 40,
     img:
       "http://www.gstatic.com/hostedimg/006cf1ebee5f652d_landing",
-    amount: 1
+    amount: 1,
+    stock: 10,
+    type: 'vegetal'
   },
   {
-    id: 4,
-    title: "Tomate",
+    id: 5,
+    title: "Apple",
     price: 40,
     img:
-      "http://www.gstatic.com/hostedimg/f6158b1deb80b4ed_landing",
-    amount: 0
-  }
+      "https://media.istockphoto.com/photos/freshly-picked-red-apples-in-a-wicker-basket-picture-id1255425952?b=1&k=6&m=1255425952&s=170667a&w=0&h=WjiaQgGHCHlEjLycpAKvSABwQ9y5cn1YGSzCg1UdbUw=",
+    amount: 0,
+    stock: 10,
+    type: 'fruta'
+  },
+  {
+    id: 6,
+    title: "Grapes",
+    price: 40,
+    img:
+      "https://media.istockphoto.com/photos/black-grapes-picture-id171151560?b=1&k=6&m=171151560&s=170667a&w=0&h=OIAl9as7YumZiP1Fld84FEzhDwH13HqmmIuR7IwMvIs=",
+    amount: 0,
+    stock: 10,
+    type: 'fruta'
+  },
+
 ];
